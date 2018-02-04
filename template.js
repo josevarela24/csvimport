8a61dd71708e4deee0f8735d3ab400b6b9ebeff1
Jose Varela <jose.varela24@yahoo.com>
2018-02-04 02:51:15 -0600

    altered from authors to countries

--- template.js
+++ template.js
@@ -4,17 +4,24 @@ exports.get = function(req, res) {
 
 	//var fields = Object.keys(Author.schema.obj);
 	var fields = [
-		'name.firstName',
-		'name.lastName',
-		'biography',
-		'twitter',
-		'facebook',
-		'linkedin'
+		'name',
+		'year',
+		'nominalGDP',
+		'realGDP',
+		'nominalGDPGrowth',
+		'realGDPGrowth',
+		'gdpPpp',
+		'population',
+		'populationGrowth',
+		'consumerSpending',
+		'easeOfDoingBusiness',
+		'unemployment',
+		'retailSalesGrowth'
 	];
 
 	var csv = json2csv({ data: '', fields: fields });
 
-	res.set("Content-Disposition", "attachment;filename=authors.csv");
+	res.set("Content-Disposition", "attachment;filename=countries.csv");
 	res.set("Content-Type", "application/octet-stream");
 
 	res.send(csv);
--- upload.js
+++ upload.js
@@ -1,38 +1,70 @@
 var csv = require('fast-csv');
 var mongoose = require('mongoose');
-var Author = require('./author');
+//var Author = require('./author');
+var Country = require('./country');
 //*fixed..this upload does not delete what is already inside db, need to implement drop feature
+
+// exports.post = function (req, res) {
+// 	if (!req.files)
+// 		return res.status(400).send('No files were uploaded.');
+	
+// 	var authorFile = req.files.file;
+
+// 	var authors = [];
+		
+// 	csv
+// 	 .fromString(authorFile.data.toString(), {
+// 		 headers: true,
+// 		 ignoreEmpty: true
+// 	 })
+// 	 .on("data", function(data){
+// 		 data['_id'] = new mongoose.Types.ObjectId();
+		 
+// 		 authors.push(data);
+// 	 })
+// 	 .on("end", function(){
+// 		//this removes documents only on first upload***
+// 		//fixed! now removes documents on each
+// 		Author.remove({}, function(err,removed) {
+
+// 		});
+// 		//db.Author.remove({});
+
+// 		 Author.create(authors, function(err, documents) {
+// 			if (err) throw err;
+			
+// 			res.send(authors.length + ' authors have been successfully uploaded.');
+// 		 });
+// 	 });
+// };
 
 exports.post = function (req, res) {
 	if (!req.files)
 		return res.status(400).send('No files were uploaded.');
 	
-	var authorFile = req.files.file;
-
-	var authors = [];
+	var countryFile = req.files.file;
+	//console.log(countryFile);
+	var countries = [];
 		
 	csv
-	 .fromString(authorFile.data.toString(), {
+	 .fromString(countryFile.data.toString(), {
 		 headers: true,
 		 ignoreEmpty: true
 	 })
 	 .on("data", function(data){
 		 data['_id'] = new mongoose.Types.ObjectId();
 		 
-		 authors.push(data);
+		 countries.push(data);
 	 })
 	 .on("end", function(){
 		//this removes documents only on first upload***
 		//fixed! now removes documents on each
-		Author.remove({}, function(err,removed) {
+		Country.remove({}, function(err,removed) {});
 
-		});
-		//db.Author.remove({});
-
-		 Author.create(authors, function(err, documents) {
+		 Country.create(countries, function(err, documents) {
 			if (err) throw err;
-			
-			res.send(authors.length + ' authors have been successfully uploaded.');
+			console.log(countries);
+			res.send(countries.length + ' countries have been successfully uploaded.');
 		 });
 	 });
 };
